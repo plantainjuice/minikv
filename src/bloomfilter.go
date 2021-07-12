9c9e20a90f6a1ee6b99b9d57959425bb957ee781
fireflychen <531109985@qq.com>
2021-07-12 23:40:26 +0800

    add bloomfilter

--- src/bloomfilter.go
+++ src/bloomfilter.go
@@ -1,33 +1,44 @@
-package bloomfilter
-
-import "fmt"
+package src
 
 type BloomFilter struct {
 	K          int
 	BitsPerKey int
-	BitLen     int
-	Result     []byte
+	bitLen     int
+	result     []byte
 }
 
-func (bf* BloomFilter) Generate(keys [][]byte) []byte {
+func (bf *BloomFilter) Generate(keys [][]byte) []byte {
 
-	bitLen := len(keys) * bf.BitsPerKey
+	bf.bitLen = len(keys) * bf.BitsPerKey
 
 	// align the bitLen.
-	bitLen = ((bitLen + 7) / 8) << 3
-	if bitLen < 64 {
-		bitLen = 64
-	} 
-
-	bf.Result = make([]byte, (bitLen >> 3))
-
-	for i := range keys {
-		fmt.Println(len(keys[i]))
+	bf.bitLen = ((bf.bitLen + 7) / 8) << 3
+	if bf.bitLen < 64 {
+		bf.bitLen = 64
 	}
 
-	return nil
+	bf.result = make([]byte, bf.bitLen>>3)
+	for i := 0; i < len(keys); i++ {
+		h := Hash(keys[i])
+		for t := 0; t < bf.K; t++ {
+			idx := (h%bf.bitLen + bf.bitLen) % bf.bitLen // 获取索引位
+			bf.result[idx/8] |= 1 << (idx % 8) // why?
+			delta := (h >> 17) | (h << 15)
+			h += delta
+		}
+	}
+	return bf.result
 }
 
-func Contains(key []byte) bool {
-	return false
+func (bf *BloomFilter)Contains(key []byte) bool {
+	 h := Hash(key)
+	for t := 0; t < bf.K; t++ {
+		idx := (h % bf.bitLen + bf.bitLen) % bf.bitLen;
+		if (bf.result[idx / 8] & (1 << (idx % 8))) == 0 {
+			return false
+		}
+		delta := (h >> 17) | (h << 15)
+		h += delta
+	}
+	return true
 }
--- test/bloomfilter_test.go
+++ test/bloomfilter_test.go
@@ -2,11 +2,24 @@ package test
 
 import (
 	"fmt"
+	"minikv/src"
 	"testing"
 )
 
 func TestBloomFilter(t *testing.T) {
 
-	fmt.Println("li")
+	var bf = &src.BloomFilter{K: 3,BitsPerKey:10}
+
+	keys := []string{ "hello world", "hi", "bloom", "filter", "key", "value", "1", "value" };
+
+	 keyBytes :=  make([][]byte, len(keys))
+	for  i := 0; i < len(keys); i++ {
+		keyBytes[i] = []byte(keys[i])
+	}
+	bf.Generate(keyBytes)
+
+	println(bf.Contains([]byte("hi")))
+
+	fmt.Println("test over")
 
 }
